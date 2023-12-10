0d1bb562261a33250042251b8954d6a13467e1f9
Qunthunnan <bredtv6@gmail.com>
2023-12-11 01:12:46 +0300

    10.12.23 tabs - done

---
+++ src/js/main.js
@@ -0,0 +1,34 @@
+function tabsHide() {
+    sliders.forEach((element) => {
+        element.classList.remove('tabcontent--active');
+        element.classList.remove('tabcontent--show');
+    });
+}
+
+function menuSwitch(item) {
+    tabItems.forEach((element)=>{
+        element.classList.remove('tabheader__item--active');
+    });
+    item.classList.add('tabheader__item--active');
+}
+
+function tabShow(item) {
+    item.classList.add('tabcontent--show');
+}
+
+const sliders = document.querySelectorAll('.tabcontent'),
+      tabItems = document.querySelectorAll('.tabheader__item'),
+      tabWrapper = document.querySelector('.tabheader__items');
+
+      tabWrapper.addEventListener('click', (e)=>{
+        target = e.target;
+        if(target && target.matches('.tabheader__item')) {
+            tabItems.forEach((element, i)=>{
+                if(element == target) {
+                    menuSwitch(element);
+                    tabsHide();
+                    tabShow(sliders[i]);
+                }
+            });
+        }
+      });
