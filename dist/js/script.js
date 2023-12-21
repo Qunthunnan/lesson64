c570738dde044ff306a9be0a225620b275e71936
Qunthunnan <bredtv6@gmail.com>
2023-12-21 16:18:43 +0300

    calculator added

--- dist/js/script.js
+++ dist/js/script.js
@@ -377,6 +377,100 @@ class Slider {
     }
   }
 }
+
+// by Harris Benedict formula
+class CalloryCalc {
+  constructor(mainElement, keys) {
+    try {
+      this.mainElement = mainElement;
+      this.classSelector = this.mainElement.classList[0];
+      if (mainElement.querySelector(`.${this.classSelector}__choose-item`)) {} else throw "choose-item not found";
+      if (this.keysChecker(keys)) {
+        this.keys = keys;
+      } else return undefined;
+      if (this.mainElement.querySelector(`#gender .${this.classSelector}__choose-item_active`)) {
+        this.maleValues = this.keys[this.mainElement.querySelector(`#gender .${this.classSelector}__choose-item_active`).getAttribute('data-key')];
+      } else this.maleValues = [];
+      if (this.mainElement.querySelector(`#activity .${this.classSelector}__choose-item_active`)) {
+        this.activityValue = this.keys[this.mainElement.querySelector(`#activity .${this.classSelector}__choose-item_active`).getAttribute('data-key')];
+      } else this.activityValue = 0;
+      this.mainElement.addEventListener('click', e => {
+        this.chooseKey(e);
+      });
+      this.height = 0;
+      this.weight = 0;
+      this.age = 0;
+      this.paramInputs = mainElement.querySelectorAll(`input.${this.classSelector}__choose-item`);
+      this.calcResult = mainElement.querySelector(`.${this.classSelector}__result span`);
+      this.paramInputs.forEach(element => {
+        element.addEventListener('input', e => {
+          this.inputingParams(e);
+        });
+      });
+      this.calcData();
+    } catch (error) {
+      console.error(error);
+      return undefined;
+    }
+  }
+  chooseKey(e) {
+    if (e.target && e.target.matches(`.${this.classSelector}__choose-item`) && e.target.nodeName !== 'INPUT' && Object.keys(this.keys).includes(e.target.getAttribute('data-key'))) {
+      if (e.target.getAttribute('data-key') === Object.keys(this.keys)[0] || e.target.getAttribute('data-key') === Object.keys(this.keys)[1]) {
+        this.maleValues = this.keys[e.target.getAttribute('data-key')];
+        this.mainElement.querySelector(`#gender .${this.classSelector}__choose-item_active`).classList.remove(`${this.classSelector}__choose-item_active`);
+      } else {
+        this.activityValue = this.keys[e.target.getAttribute('data-key')];
+        this.mainElement.querySelector(`#activity .${this.classSelector}__choose-item_active`).classList.remove(`${this.classSelector}__choose-item_active`);
+      }
+      e.target.classList.add(`${this.classSelector}__choose-item_active`);
+      this.calcData();
+    }
+  }
+  inputingParams(e) {
+    try {
+      const validatios = {
+        age: /^[1-9]$|^([1-9]\d)$|^(1[0-3]\d)$/,
+        weight: /^[1-9]$|^([1-9]\d)$|^([1-2][0-9]\d)$/,
+        height: /^([5-9]\d)$|^([1-2][0-9]\d)$/
+      };
+      let attribute = e.target.getAttribute('id'),
+        validation = validatios[attribute];
+      if (validation.test(e.target.value)) {
+        e.target.classList.remove(`${this.classSelector}__input_error`);
+        this[attribute] = +e.target.value;
+      } else {
+        e.target.classList.add(`${this.classSelector}__input_error`);
+        this[attribute] = 0;
+      }
+      this.calcData();
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+  keysChecker(keys) {
+    let counter = 0;
+    try {
+      for (let i in keys) {
+        if (this.mainElement.querySelector(`[data-key=${i}]`)) {
+          counter++;
+        } else throw `Element with data-key attribute is not found`;
+      }
+      if (counter > 0) return true;
+    } catch (error) {
+      console.error(error);
+      return false;
+    }
+  }
+  calcData() {
+    if (this.maleValues[0] && this.activityValue > 0 && this.age > 0 && this.height > 0 && this.weight > 0) {
+      let result = Math.floor(this.maleValues[0] + this.maleValues[1] * this.weight + this.maleValues[2] * this.age * this.activityValue);
+      this.calcResult.textContent = result;
+    } else {
+      this.calcResult.textContent = '___';
+    }
+  }
+}
 function makeFoodMenu() {
   // getData('http://localhost:3000/menu')
   axios.get('http://localhost:3000/menu').then(result => {
@@ -407,7 +501,16 @@ const sliders = document.querySelectorAll('.tabcontent'),
   prevSlideBtn = document.querySelector('.offer__slider-prev'),
   curSliderCounter = document.querySelector('#current'),
   totalSliderCounter = document.querySelector('#total'),
+  calorySectElement = document.querySelector('.calculating'),
   slider = new Slider(sliderTrack, nextSlideBtn, prevSlideBtn, curSliderCounter, totalSliderCounter);
+calculator = new CalloryCalc(calorySectElement, {
+  male: [66, 13.7, 5, 6.7],
+  female: [665, 13.7, 5, 6.7],
+  low: 1,
+  small: 1.2,
+  medium: 1.3,
+  high: 1.8
+});
 let endTime = new Date('2023-12-31'),
   showModalInvId,
   usdRate,
